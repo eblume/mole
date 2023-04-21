b8935f0704dad0fbf69e60a5456fda74c059380f
Erich Blume <blume.erich@gmail.com>
2023-04-21 10:23:25 -0700

    Work VPN check

--- src/mole/jira.py
+++ src/mole/jira.py
@@ -3,6 +3,7 @@ from jira import JIRA, Issue
 
 from .todoist import TodoistRemote
 from .models import Task
+from .vpn import is_vpn_connected
 
 
 API_KEY = os.getenv('JIRA_APPLICATION_KEY')
@@ -13,10 +14,17 @@ class JiraException(Exception):
     pass
 
 
+def ensure_jira_ready():
+    if not URL:
+        raise JiraException("JIRA_SERVER_URL must be set")
+    elif not API_KEY:
+        raise JiraException("JIRA_APPLICATION_KEY must be set")
+    elif not is_vpn_connected():
+        raise JiraException("VPN must be connected")
+
+
 def get_my_issues() -> dict[str, str]:
-    if not URL or not API_KEY:
-        raise JiraException("JIRA_APPLICATION_KEY and JIRA_SERVER_URL must be set")
-        
+    assert URL is not None  # checked by ensure_jira_ready
     jira = JIRA(server=URL, token_auth=API_KEY) 
     issues = jira.search_issues('assignee = currentUser()')
 
@@ -26,6 +34,7 @@ def get_my_issues() -> dict[str, str]:
 
 def check_jira(remote: TodoistRemote) -> None:
     """Update tasks based on Jira status"""
+    ensure_jira_ready()
     issues = get_my_issues()
     jira_tasks = {t.name: t for t in remote.get_tasks(filter="(today | overdue)", label="jira")}
     extra_tasks = set(jira_tasks.keys()) - set(issues.keys())
