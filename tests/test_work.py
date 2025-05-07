7bf60711db2c10671f5b0abbbec13cc1c1a75f23
Erich Blume <blume.erich@gmail.com>
2025-05-07 15:36:16 -0700

    Use fixtures for work output

--- tests/test_work.py
+++ tests/test_work.py
@@ -1,9 +1,22 @@
-from pytest import CaptureFixture
+import pytest
+from _pytest.capture import CaptureResult
+
 
 from mole.work import work
 
 
-def test_all_done(capsys: CaptureFixture):
+@pytest.fixture
+def work_capsys(capsys: pytest.CaptureFixture[str]) -> CaptureResult[str]:
+    """This fixture actually runs the proposed work"""
+    # It is intended that this fixture will be highly parameterized for these tests.
     work()
-    captured = capsys.readouterr()
-    assert captured.out.endswith("All Done!\n")
+    return capsys.readouterr()
+
+
+@pytest.fixture
+def captured_output(work_capsys: CaptureResult[str]) -> str:
+    return work_capsys.out
+
+
+def test_all_done(captured_output: str):
+    assert captured_output.endswith("All Done!\n")
