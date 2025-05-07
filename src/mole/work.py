c279056ef2be34e764e2b883a0d0aaecdc22167d
Erich Blume <blume.erich@gmail.com>
2025-05-07 15:51:38 -0700

    Test we can work on some chores

--- src/mole/work.py
+++ src/mole/work.py
@@ -1,2 +1,7 @@
-def work() -> None:
+from mole.task import ChoreDefinition
+
+
+def work(chores: list[ChoreDefinition] | None = None) -> None:
+    for chore in chores or []:
+        print(chore.name)
     print("All Done!")
